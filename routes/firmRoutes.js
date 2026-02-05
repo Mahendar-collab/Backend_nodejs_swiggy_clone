84c772237e09cd3bba640e0406776cf00dff9d66
mahendar <mahendarn915@gmail.com>
2026-02-05 09:44:33 +0530

    firm route updated

--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -1,19 +1,34 @@
 const express = require('express');
+const path = require('path');
+const multer = require('multer');
 const firmController = require('../controllers/firmController');
 const verifyToken = require('../middlewares/verifyToken');
 
+const router = express.Router();
 
-const router = express.Router()
+// multer config
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
 
-router.post('/add-firm', verifyToken, firmController.addFirm);
+const upload = multer({ storage });
+
+router.post(
+  '/add-firm',
+  verifyToken,
+  upload.single('image'), // ✅ multer BEFORE controller
+  firmController.addFirm
+);
 
 router.get('/uploads/:imageName', (req, res) => {
-    const imageName = req.params.imageName;
-    res.header('Content-Type', 'image/jpeg');
-    res.sendFile(path.join(__dirname, '..', 'uploads', imageName));
+  res.sendFile(path.join(__dirname, '..', 'uploads', req.params.imageName));
 });
 
 router.delete('/:firmId', firmController.deleteFirmById);
-
 
 module.exports = router;
